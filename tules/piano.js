379108c89cdda478bd7086fbb97b1799d0d1f459
Mozeick <lalnunhlima247@gmail.com>
2026-03-17 15:09:36 +0530

    Update piano.js

--- tules/piano.js
+++ tules/piano.js
@@ -11,6 +11,7 @@ html, body { height: 100%; margin: 0; padding: 0; overflow-x: hidden; box-sizing
   height: 60%;
   display: flex;
    flex-direction: column;
+background: black;
 }
 
  @media (orientation: portrait) {
